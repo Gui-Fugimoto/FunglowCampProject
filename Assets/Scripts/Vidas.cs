36d40d59fea9d3a8c54b16b5aa0a942dfd4da5dc
NickolasSilveira <chemicalcake@outlook.com>
2021-06-01 10:03:37 -0300

    Protótipo Vidas
    Implementação do sistema de vida do personagem

---
+++ Assets/Scripts/Vidas.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Vidas : MonoBehaviour
+{
+    public int vidaMaxima;
+    public int vidaAtual;
+
+    
+    public Image tresVidas;
+    public Image duasVidas;
+    public Image umaVida;
+
+    public string SampleScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaAtual = vidaMaxima;
+        tresVidas.enabled = true;
+        duasVidas.enabled = true;
+        umaVida.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LevouDano()
+    {
+        vidaAtual -= 1;
+
+        if (vidaAtual == 3)
+        {
+            tresVidas.enabled = true;
+            duasVidas.enabled = true;
+            umaVida.enabled = true;
+        }
+
+        if (vidaAtual == 2)
+        {
+            tresVidas.enabled = false;
+            duasVidas.enabled = true;
+            umaVida.enabled = true;
+        }
+
+        if (vidaAtual == 1)
+        {
+            tresVidas.enabled = false;
+            duasVidas.enabled = false;
+            umaVida.enabled = true;
+        }
+
+        if (vidaAtual <= 0)
+        {
+            Debug.Log("perdeu");
+            SceneManager.LoadScene(SampleScene);
+        }
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Inimigo"))
+        {
+            Debug.Log("encostou");
+            LevouDano();
+        }
+    }
+}
+
