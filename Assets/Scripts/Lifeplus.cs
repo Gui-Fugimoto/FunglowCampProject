0b1968898243bbdcc2d9fa07a4c7a627220dbc29
Arthur989828 <69467425+Arthur989828@users.noreply.github.com>
2021-06-15 15:54:45 -0300

    equip estilingue e shoot
    estilingue na mão do player e teste de shoot

---
+++ Assets/Scripts/Lifeplus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lifeplus : MonoBehaviour
+{
+    public Image imageMaçacura;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            imageMaçacura.enabled = false;
+            //+ meia vida ou 1 vida no HUD
+        }
+    }
+}
